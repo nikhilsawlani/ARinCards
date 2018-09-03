3d9f9b77743d2fc1db582425a0c2a6be5351701d
Nikhil <NIKHIL_SAWLANI@CRGL-THIRDPARTY.COM>
2018-09-03 21:14:49 +0530

    logos added

--- Assets/ContentChanger.cs
+++ Assets/ContentChanger.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class ContentChanger : MonoBehaviour {
 
+    public GameObject accountinfo;
+    public GameObject offers;
+    public GameObject Analytics;
+    public GameObject fd;
+    public GameObject stocks;
 	// Use this for initialization
 	void Start () {
-		
+        accountinfo.SetActive(true);
+        offers.SetActive(false);
+        Analytics.SetActive(false);
+        fd.SetActive(false);
+        stocks.SetActive(false);
 	}
 	
 	// Update is called once per frame
